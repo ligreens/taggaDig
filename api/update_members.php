22702070748430590b2e3349dca15b57fdaae012
ligreens <linagreen_@hotmail.com>
2017-03-28 20:49:08 +0200

    ändrade if-statements

--- api/update_members.php
+++ api/update_members.php
@@ -1,45 +1,19 @@
 <?php
 
-require 'session.phpp';
+require 'session.php';
 require 'database.php';
-failed_to_login();
-// UPPDATERA ANVÄNDARE
-$_SESSION['email'] = 'email';
+user_login_failed();
+
 if (isset($_POST['update'])) {
     if (isset($_SESSION['userid'])) {
-        if (isset($_POST['business']) && strlen($_POST['business']) > 0) {
-            $updateStm = $pdo->prepare("UPDATE `members` SET `business` = :business WHERE `id`= :id");
-            $updateStm->execute(['business' => $_POST['business'], 'id' => $_SESSION['userid']]);
-            // echo json_encode(true);
-        }
-        if (isset($_POST['firstName']) && strlen($_POST['firstName']) > 0) {
-            $updateStm = $pdo->prepare("UPDATE `members` SET `firstName` = :firstName WHERE `id`= :id");
-            $updateStm->execute(['firstName' => $_POST['firstName'], 'id' => $_SESSION['userid']]);
-            // echo json_encode(true);
-        }
-        if (isset($_POST['lastName']) && strlen($_POST['lastName']) > 0) {
-            $updateStm = $pdo->prepare("UPDATE `members` SET `lastName` = :lastName WHERE `id`= :id");
-            $updateStm->execute(['lastName' => $_POST['lastName'], 'id' => $_SESSION['userid']]);
-            // echo json_encode(true);
-        }
-        if (isset($_POST['email']) && strlen($_POST['email']) > 0) {
-            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-                $updateStm = $pdo->prepare("UPDATE `members` SET `email` = :email WHERE `id`= :id");
-                $updateStm->execute(['email' => $_POST['email'], 'id' => $_SESSION['userid']]);
-                // echo json_encode(true);
-            } else {
-                // echo json_encode(false);
+
+       $fields = ['business','firstName','lastName','email','phone','password'];
+        foreach ($fields as $field) {
+            if (isset($_POST[$field])&& strlen($_POST[$field]) > 0) {
+                $updateStm = $pdo->prepare("UPDATE `members` SET `{$field}` = :{$field} WHERE `id`= :id");
+                $updateStm->execute(["{$field}" => $_POST[$field], 'id' => $_SESSION['userid']]);
+                echo json_encode(true);
             }
-        }
-        if (isset($_POST['phone']) && strlen($_POST['phone']) > 0) {
-            $updateStm = $pdo->prepare("UPDATE `members` SET `phone` = :phone WHERE `id`= :id");
-            $updateStm->execute(['phone' => $_POST['phone'], 'id' => $_SESSION['userid']]);
-            echo json_encode(true);
-        }
-        if (isset($_POST['password']) && strlen($_POST['password']) > 0) {
-            $updateStm = $pdo->prepare("UPDATE `members` SET `password` = :password WHERE `id`= :id");
-            $updateStm->execute(['password' => $_POST['password'], 'id' => $_POST['update']]);
-            // echo json_encode(true);
         }
     }
 }
