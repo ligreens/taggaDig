62d7b2a605f335012e6da245d6751f72068ca02e
ligreens <linagreen_@hotmail.com>
2017-03-28 19:21:16 +0200

    Fixade start_page.php

--- api/start_page.php
+++ api/start_page.php
@@ -5,15 +5,3 @@ require 'database.php';
 echo error_message();
 // kalla på sessionfunktion
 session_destroy();
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-<meta charset="utf-8">
-    </head>
-    <body>
-<form action="login.php" method="POST" name="login">
-     <input type="text" name="email" value="" placeholder="E-post" /><br>
-     <input type="password" name="password" value="" placeholder="Lösenord"/><br>
-    <button type="submit" name="login">Logga in</button>
-</form>
